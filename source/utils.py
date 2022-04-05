1dc0d9ca660736d1468f8fbe03556969bb8f54f8
snatchanon <few.natchanon@gmail.com>
2022-04-05 22:27:22 +0700

    move func to helper

--- source/utils.py
+++ source/utils.py
@@ -1,10 +1,9 @@
 import numpy as np
 import os, json
 from collections import Counter
-import torch
 import os
 
-def prepare_data(img_path, json_file):
+def prepare_data(img_path, json_file, vocab_threshold):
     # Read data split from json
     with open(json_file, 'r') as j:
         data = json.load(j)
@@ -35,7 +34,7 @@ def prepare_data(img_path, json_file):
             test_x.append(full_path)
             test_y.append(caption)
 
-    vocab = [word for word in word_freq.keys() if word_freq[word] > 3]
+    vocab = [word for word in word_freq.keys() if word_freq[word] > vocab_threshold]
     vocab_dict = {key:value+4 for value, key in enumerate(vocab)}
     vocab_dict["<unk>"] = 3
     vocab_dict["<start>"] = 1
@@ -51,53 +50,21 @@ def prepare_data(img_path, json_file):
 
 def Create_image_caption_pair(image_split, caption_split, vocab, max_len=50):
     enc_caption_list = []
+    enc_caption_len = []
     img_list = []
     for idx, img in enumerate(image_split):
         for cap in caption_split[idx]:
+            # Dont sample caption more than max_len
+            if len(cap) > max_len:
+                continue
             # Word tokenization
             enc_cap = [vocab['<start>']] + [vocab.get(word, vocab['<unk>']) for word in cap] + \
                         [vocab['<end>']] + ([vocab['<pad>']] * (max_len - len(cap)))
             # Append to global list
             enc_caption_list.append(enc_cap)
+            enc_caption_len.append(len(cap)+2)
             img_list.append(img)
 
-    return img_list, enc_caption_list
+    return img_list, enc_caption_list, enc_caption_len
 
 
-def train(encoder, decoder, device, train_loader, optimizer, criterion, vocab_size):
-    total_loss = 0
-    encoder.train()
-    decoder.train()
-    for idx, data in enumerate(train_loader):
-        data_img = data[0].to(device)
-        data_cap = data[1].to(device)
-        encoder.zero_grad()
-        decoder.zero_grad()
-        img_latent = encoder(data_img)
-        output = decoder(img_latent, data_cap)    
-        loss = criterion(output.view(-1, vocab_size), data_cap.view(-1).long())
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
- 
-    total_loss /= len(train_loader)
-
-    return total_loss
-        
-
-# Test
-def evaluate(encoder, decoder, device, test_loader, vocab_size):
-    encoder.eval()
-    decoder.eval()
-    total_preds = []
-    total_labels = []
-    with torch.no_grad():
-        for data in test_loader:
-            data_img = data[0].to(device)
-            data_cap = data[1]
-            img_latent = encoder(data_img)
-            outputs = decoder.inference(img_latent)
-            total_preds.append(outputs)
-            total_labels.append(data_cap)
-
-    return total_preds, total_labels
