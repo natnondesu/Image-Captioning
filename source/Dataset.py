f149bfec6e2e46ee14edc4ac824c35b0fd27c095
snatchanon <few.natchanon@gmail.com>
2022-04-25 19:15:48 +0700

    Also return data idx

--- source/Dataset.py
+++ source/Dataset.py
@@ -4,10 +4,11 @@ from torchvision import transforms
 from PIL import Image
 
 class ImgCaption_Dataset(Dataset):
-    def __init__(self, img_path, caption, caption_length, transform=None):
+    def __init__(self, img_path, caption, caption_length, cap_idx, transform=None):
         self.img_path = img_path
         self.y = torch.LongTensor(caption)
         self.caplen = caption_length
+        self.cap_idx = cap_idx
         self.transform = transform
 
     def __getitem__(self, index):
@@ -15,7 +16,8 @@ class ImgCaption_Dataset(Dataset):
         x = self.transform(img)
         y = self.y[index] # Retrieve data
         caplen = self.caplen[index]
-        return x, y, caplen
+        cap_idx = self.cap_idx[index]
+        return x, y, caplen, cap_idx
 
     def __len__(self):
         return self.y.shape[0]
