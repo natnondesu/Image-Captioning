5e63949e52c863f99c5e05eb511d1f217df16b8f
tutorgaming <tutorgaming@gmail.com>
2022-05-21 22:33:36 +0700

    Add Evaluation by BLEU2-4 in Evaluate with corpus_bleu
    - Fix evaluate returning only one value (from GIST)
    - Remove Some trailing whitespace

--- source/helper.py
+++ source/helper.py
@@ -18,18 +18,17 @@ def train(encoder, decoder, device, train_loader, optimizer, criterion, log_inte
         img_latent = encoder(data_img)
         outputs, decoded_lengths, att = decoder(img_latent, data_cap, caption_length)
         packed_output = pack_padded_sequence(outputs, decoded_lengths, batch_first=True, enforce_sorted=False)
-        packed_label = pack_padded_sequence(data_cap, decoded_lengths, batch_first=True, enforce_sorted=False)  
+        packed_label = pack_padded_sequence(data_cap, decoded_lengths, batch_first=True, enforce_sorted=False)
         loss = criterion(packed_output[0], packed_label[0])
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
         if idx%log_interval == 0:
             print(f"Train step [{idx} / {len(train_loader)}], loss : ", loss.cpu().detach().numpy().item())
- 
+
     total_loss /= len(train_loader)
 
     return total_loss
-        
 
 # Test
 def evaluate(encoder, decoder, device, test_loader, vocab_dict, caption):
@@ -37,7 +36,10 @@ def evaluate(encoder, decoder, device, test_loader, vocab_dict, caption):
     decoder.eval()
     total_preds = []
     total_labels = []
-    total_bleu = []
+    # n-BLEU Score on Each n-Gram
+    total_bleu2 = []
+    total_bleu3 = []
+    total_bleu4 = []
 
     with torch.no_grad():
         for idx, data in enumerate(test_loader):
@@ -50,11 +52,21 @@ def evaluate(encoder, decoder, device, test_loader, vocab_dict, caption):
             outputs = outputs.tolist()
             hypotheses = get_caption_back(outputs, vocab_dict)
             references = [caption[i] for i in caption_idx]
-            
-            bleu4 = corpus_bleu(references, hypotheses)
-            total_bleu.append(bleu4)
+            # Calculate BLEU Score
+            bleu2, bleu3 ,bleu4 = corpus_bleu(references, hypotheses, weights=[
+                (0.5, 0.5),              # For BLEU-2
+                (0.333, 0.333, 0.334),   # For BLEU-3
+                (0.25, 0.25, 0.25, 0.25) # For BLEU-4
+            ])
+            total_bleu2.append(bleu2)
+            total_bleu3.append(bleu3)
+            total_bleu4.append(bleu4)
+    # Mean of n-BLEU Score for test set
+    mean_bleu2 = np.array(total_bleu2).mean()
+    mean_bleu3 = np.array(total_bleu3).mean()
+    mean_bleu4 = np.array(total_bleu4).mean()
 
-    return np.array(total_bleu).mean()
+    return mean_bleu2, mean_bleu3, mean_bleu4
 
 def LR_scheduler_with_warmup(optimizer, LR, epoch, warmup_epoch=0, scale=0.7, set_LR=0.001, interval_epoch=2):
     """Sets the learning rate to the initial LR decayed by 5% every interval epochs"""
@@ -64,7 +76,7 @@ def LR_scheduler_with_warmup(optimizer, LR, epoch, warmup_epoch=0, scale=0.7, se
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
         return lr
-        
+
     elif (epoch % interval_epoch) == 0:
         lr = np.around(LR * scale, decimals=5)
         for param_group in optimizer.param_groups:
